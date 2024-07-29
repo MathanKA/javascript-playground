2bf3f56821c75e3ca1f68a1e2f1bf74daf2bc717
Mathan K A <mathanka.live@gmail.com>
2024-07-29 18:34:31 +0000

    null vs undefined

---
+++ index.js
@@ -0,0 +1,29 @@
+let var1;
+let var2 = null;
+
+let object = {
+  a: 1,
+  b: undefined
+};
+
+function test(arg1, arg2) {
+  console.log(arg1);  // undefined: not provided
+  console.log(arg2);  // null: provided as such
+}
+
+function clearProperty(prop) {
+    console.log(object[prop])
+  delete object[prop];
+}
+
+console.log(var1);     // undefined
+console.log(var2);     // null
+console.log(object.a); // 1
+console.log(object.b); // undefined
+console.log(object.c); // undefined
+
+test();               // Both arguments are undefined
+test(1, null);        // arg1 is 1, arg2 is null
+
+clearProperty('c');  // Removes property 'b' from object
+console.log(object.b); // undefined: Property 'b' was removed, not set to null
