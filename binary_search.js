fa1ad1b356fe37058c1588bbb65fd3482338b6bd
Mathan K A <amheretoinspire@hotmail.com>
2024-08-09 14:44:58 +0000

    binary

--- binary_search.js
+++ binary_search.js
@@ -5,9 +5,10 @@ var doSearch = function (array, targetValue) {
   var max = array.length - 1;
   var guess;
 
-  while (max > min) {
+  while (max >= min) {
     guess = Math.floor((min + max) / 2);
     if (array[guess] === targetValue) {
+      console.log(guess);
       return guess;
     } else if (array[guess] < targetValue) {
       min = guess + 1;
@@ -15,7 +16,10 @@ var doSearch = function (array, targetValue) {
       max = guess - 1;
     }
   }
+  console.log("nope");
   return -1;
 };
 
-doSearch([2, 6, 8, 10, 13, 15, 17, 20], 13);
+doSearch([1, 3, 5, 7, 9, 11, 13, 15], 7);
+
+doSearch( [20,30,40,50,60,70,80,90] ,20);
